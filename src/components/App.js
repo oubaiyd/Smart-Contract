298fdc008c074fc46708697b9814b5bc798411e5
Oubaidallah <ayadioubaidallah@gmail.com>
2022-08-15 17:54:18 +0200

    [FIX] fix import error

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import Web3 from "web3";
-import Ouba from "./build/Ouba.json";
-import Rewards from "./build/Rewards.json";
-import Staking from "./build/Staking.json";
+import Ouba from "../build/Ouba.json";
+import Rewards from "../build/Rewards.json";
+import Staking from "../build/Staking.json";
 import "./App.css";
 import { ethers } from "ethers";
 
